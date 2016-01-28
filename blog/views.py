844855b6be513c2e6153072a06212bf6de545009
weiwei.huang <18686672327@163.com>
2016-01-28 22:09:08 +0800

    first init

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from blog.models import BlogPost
+
+# Create your views here.
+
+def index(request):
+	blog_list = BlogPost.objects.all()
+	return render(request, 'index.html', {'blog_list' : blog_list})
